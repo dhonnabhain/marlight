0ec186038270af98671767dfc16df42b747d9915
Donovan <donovan.broquin@outlool.com>
2022-05-28 11:48:20 +0200

    feat: add comics load by serie

--- stores/comics.js
+++ stores/comics.js
@@ -9,7 +9,7 @@ export const useComicsStore = defineStore({
   },
   actions: {
     async fetch(config) {
-      if (this.comics[config.id]) return;
+      if (this.comics[config.id]) return this.comics[config.id];
 
       try {
         const {
@@ -17,8 +17,21 @@ export const useComicsStore = defineStore({
         } = await useApi({ id: config.id, entity: "comics" });
 
         this.comics[config.id] = results[0];
-        
-        return results[0]
+
+        return results[0];
+      } catch (e) {
+        console.log("Ooops", e);
+      }
+    },
+    async loadFromSerie({ id, entity, limit }) {
+      try {
+        const {
+          data: { results },
+        } = await useApi({ id, entity, limit, suffix: "comics" });
+
+        results.forEach((comic) => (this.comics[comic.id] = comic));
+
+        return results;
       } catch (e) {
         console.log("Ooops", e);
       }
