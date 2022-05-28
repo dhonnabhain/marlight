ea732b85f07a5e1af227d213b3cd9358d39573d9
Donovan <donovan.broquin@outlool.com>
2022-05-28 11:49:13 +0200

    chore: series and comics should be under illustration / title

--- pages/series/[id].vue
+++ pages/series/[id].vue
@@ -2,34 +2,36 @@
   <main v-if="serie" class="sm:w-2/3 sm:mx-auto">
     <Back />
 
-    <article class="p-8 sm:flex sm:gap-8">
-      <img
-        :src="thumbnail"
-        :alt="`${serie.title} picture`"
-        class="block object-cover rounded-lg sm:w-96 sm:self-start drop-shadow-xl"
-      />
-
-      <div class="flex flex-col">
-        <h3 class="text-lg leading-6 font-medium text-gray-900">
-          {{ serie.title }}
-        </h3>
-
-        <p class="prose prose-sm prose-slate dark:prose-invert mt-4">
-          {{ serie.description }}
-        </p>
-
-        <ExternalLink
-          :url="urls.detail"
-          label="Read more"
-          icon="arrow-up-right-from-square"
-          class="mt-4 w-full sm:w-auto sm:self-start"
+    <article class="p-8">
+      <div class="sm:flex sm:gap-8">
+        <img
+          :src="thumbnail"
+          :alt="`${serie.title} picture`"
+          class="block object-cover rounded-lg sm:w-96 sm:self-start drop-shadow-xl"
         />
 
-        <ComicsList
-          :comics="serie.comics.items"
-          :count="serie.comics.available"
-        />
+        <div class="flex flex-col mt-6">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">
+            {{ serie.title }}
+          </h3>
+
+          <p
+            v-if="serie.description !== ''"
+            class="prose prose-sm prose-slate dark:prose-invert mt-3 sm:mt-4"
+          >
+            {{ serie.description }}
+          </p>
+
+          <ExternalLink
+            :url="urls.detail"
+            label="Read more"
+            icon="arrow-up-right-from-square"
+            class="mt-4 w-full sm:w-auto sm:self-start"
+          />
+        </div>
       </div>
+
+      <ComicsList :parent="serie" :count="serie.comics.available" entity="series" />
     </article>
   </main>
 </template>
