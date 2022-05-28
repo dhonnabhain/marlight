b897d61764443fd30f3abc2849283fde089ddb97
Donovan <donovan.broquin@outlool.com>
2022-05-28 11:50:31 +0200

    feat: add comic placeholder

--- components/Placeholder.vue
+++ components/Placeholder.vue
@@ -1,7 +1,21 @@
 <template>
   <li
+    v-if="comic"
+    class="w-full rounded-md truncate hover:text-white flex justify-center animate-pulse"
+  >
+    <div class="w-32 h-48 bg-slate-400 rounded" />
+  </li>
+
+  <li
+    v-else
     class="relative col-span-1 flex flex-col justify-end items-center p-2 pb-6 animate-pulse bg-slate-300 h-48 w-full rounded-lg"
   >
     <div class="bg-slate-400 w-2/3 h-3 rounded"></div>
   </li>
 </template>
+
+<script setup>
+defineProps({
+  comic: { type: Boolean, default: false },
+});
+</script>
